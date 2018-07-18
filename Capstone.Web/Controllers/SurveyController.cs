10751f24c2e3a261f566b876437a0dfd04e593dc
Bakhary Drummond <bdrummond3625@bths.edu>
2018-07-18 11:01:34 -0400

    Writing logic for survey page

---
+++ Capstone.Web/Controllers/SurveyController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class SurveyController : Controller
+    {
+
+        /// <summary>
+        /// Display all the previous survey results.
+        /// </summary>
+        /// <returns>The page with the results bound to it.</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Capstone.Web/Models/SurveySqlDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class SurveySqlDAL
+    {
+        private string ConnectionString;
+
+        public SurveySqlDAL (string constring)
+        {
+            this.ConnectionString = constring;
+        }
+
+        public IList<Survey> GetAllResults()
+        {
+            var results = new List<Survey>();
+            string sql = "Select * From survey_result;";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    var cmd = new SqlCommand(sql, conn);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        results.Add(MapRowToSurvey(reader));
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                throw;
+            }
+            return results;
+        }
+
+        public Survey MapRowToSurvey(SqlDataReader reader)
+        {
+            return new Survey()
+            {
+                ID =Convert.ToInt32( reader["surveyId"]),
+                ParkCode=Convert.ToString( reader["parkCode"]),
+                State = Convert.ToString(reader["state"]),
+                ActivityLevel= Convert.ToString(reader["activityLevel"]),
+                Email= Convert.ToString(reader["emailAddress"])
+
+               
+            };
+        }
+    }
+}
