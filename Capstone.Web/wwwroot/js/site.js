39f5fe7d77844754642749e925e1bf7b30d4ed4f
Ryan Hord <ryhord13@gmail.com>
2018-07-19 14:34:15 -0400

    sharpened js scripts

--- Capstone.Web/wwwroot/js/site.js
+++ Capstone.Web/wwwroot/js/site.js
@@ -1,4 +1,3 @@
-
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
@@ -17,16 +16,35 @@ function setCookie(cname, cvalue, exdays) {
 
 function alertMe() {
     var buttonTxt = document.getElementById("convertBtn");
-    if (buttonTxt.innerHTML == 'Convert To F') {
-        buttonTxt.innerHTML = 'Convert To C';
+    if (buttonTxt.innerHTML == 'Convert To C') {
+        buttonTxt.innerHTML = 'Convert To F';
+        var temps = document.getElementsByClassName("temperature");
+        for (var i = 0; i < temps.length; i++) {
+            temps[i].textContent = convertToC(temps[i].textContent);
+            console.log(temps[i]);
+            
+        }
 
     }
     else {
-        buttonTxt.innerHTML = 'Convert To F';
-
+        buttonTxt.innerHTML = 'Convert To C';
+        var temps = document.getElementsByClassName("temperature");
+        for (var i = 0; i < temps.length; i++) {
+            temps[i].textContent = convertToF(temps[i].textContent);
+            console.log(temps[i]);
+        }
     }
-
-
-    alert(buttonTxt.innerHTML);
-
 }
+
+
+
+function convertToF(num) {
+    var intNum = parseInt(num);
+    console.log("this is convert to F");
+    return intNum * 1.8 + 32;
+};
+function convertToC(num) {
+    var intNum = parseInt(num);
+    console.log("this is convert to C");
+    return (intNum - 32) / 1.8;
+};
